2acafc52fe3dc3fdab459e31ca0716dc4188efd7
coreattack22 <coreattack22@gmail.com>
2018-07-17 17:14:58 +0900

    test

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def handle_message(event):
     print (event.source)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='あいうえお'))
 
 
 if __name__ == "__main__":
