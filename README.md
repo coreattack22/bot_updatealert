615c98bd0fc49e8e150d2c69f72756330b1495af
coreattack22 <coreattack22@gmail.com>
2018-07-18 03:23:04 +0900

    temporary

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ https://shareshortbot.herokuapp.com/
 
 ##使い方
 
+このソースコードをherokuとかにデプロイする
+
+GoogleAppsScriptにgas.jsのコードをそのまま貼り付けて、トリガー(時計のマーク)
+
+で任意の時間を指定して実行する(Herokuのトリガーもあるが、動かすと無料枠圧迫するので)
+
+![Demo](https://user-images.githubusercontent.com/34805754/42837630-96ea96a8-8a39-11e8-93a9-d6a3088e503d.png)
+
+###その他
+
+時間があればもっとちゃんとした体裁に書き直したい・・・
--- gas.js
+++ gas.js
@@ -1,27 +1,25 @@
 //gasに配置するファイル参考
 
 function push(){
-    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    const sheet = spreadsheet.getSheetByName("巡回対象");
-    const range = sheet.getRange("C6:D100");
-    const values = range.getValues().filter( function( value ) {
-        return value[0] !='';
-    })
-    const id=sheet.getRange("C3").getValue();
-    Logger.log(id);
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = spreadsheet.getSheetByName("巡回対象");
+  const range = sheet.getRange("C6:D100");
+  const values = range.getValues().filter( function( value ) {
+      return value[0] !='';
+  })
+  const id=sheet.getRange("C3").getValue();
+
   
-    
-   
-    values.forEach(function(value) {
-      const site_url=value[1].toString().split('/').join('-');
+  values.forEach(function(value) {
+    var site_url=value[1].toString().split('/').join('-');
+    site_url=site_url.split('?').join('~');
+    var url="";
+    if (value[0].toString()=='ライブドアブログ'){
+      url = "https://shareshortbot.herokuapp.com/push_livedoorblog/"+site_url+"/"+id;  
+    } else if (value[0].toString()=='wantedly') {
+      url = "https://shareshortbot.herokuapp.com/push_wantedly/"+site_url+"/"+id;  
+    }
+    UrlFetchApp.fetch(url).getContentText("UTF-8");
+  });
   
-      if (value[0].toString()=='ライブドアブログ'){
-        const url = "https://shareshortbot.herokuapp.com/push_update/"+site_url+"/"+id;  
-      } else if (value[0].toString()=='wantedly') {
-        console.log("作成中")
-      }
-      const response = UrlFetchApp.fetch(url);
-      const content = response.getContentText("UTF-8");
-    });
-    
-  }
+}
